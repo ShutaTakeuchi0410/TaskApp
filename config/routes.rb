0c2ee641a9979170433a47802c8b32945f4635d1
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-12 19:07:18 +0900

    deviseを導入した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
