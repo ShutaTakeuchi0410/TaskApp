36f013bef0578c300713a54b018f9bacfdf75019
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-13 14:35:33 +0900

    taskテーブルのマイグレーション

--- db/migrate/20210613042215_create_tasks.rb
+++ db/migrate/20210613042215_create_tasks.rb
@@ -1,6 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.string :title, nil: false
+      t.datetime :deadline #締切日
+      t.text :detail #詳細
+      t.integer :priority #優先度
+
+      t.integer :user_id
 
       t.timestamps
     end
