0bccd4e7aaff84ccb8d9fda0ab205dcbfc864b77
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-13 18:10:29 +0900

    home#indexにて「締め切りが今日のタスク」を取得して表示させた

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,22 @@ class HomeController < ApplicationController
 
   # 締切日が今日でプロジェクトの依存関係は問わないタスク一覧
   def index
+    @tasks = current_user.tasks.where("deadline = ?", get_today_date)
   end
 
   # 締切日とプロジェクトが未定のタスク一覧
   def inbox
-    
+    @tasks = current_user.tasks.where(deadline: nil)
   end
 
   # 締切日が今日以外で、プロジェクトの依存関係は問わないタスク一覧
   def future
   end
 
+  private
+
+  # 本日の日付を取得
+  def get_today_date
+    today = Date.current.strftime('%Y/%m/%d')
+  end
 end
