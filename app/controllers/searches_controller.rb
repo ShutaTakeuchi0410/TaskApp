821ef058648e2b8c7e030db7f57e0a46794a9efc
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-17 08:06:45 +0900

    検索機能の修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 class SearchesController < ApplicationController
+
   # 検索フォーム用インスタンスを生成
   def new
     @search = Search.new
@@ -11,7 +12,8 @@ class SearchesController < ApplicationController
   def create
     @search = current_user.searches.new(search_params)
     if @search.save
-      redirect_to root_path
+      # 検索結果を表示したい(GETで結果を表示させる)
+      redirect_to action: :show_result
     else
       # 検索履歴５件
       @searches = current_user.searches.all.order('id DESC').limit(5)
@@ -24,5 +26,4 @@ class SearchesController < ApplicationController
   def search_params
     params.require(:search).permit(:word, :column_type)
   end
-
 end
