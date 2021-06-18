1d816a4ff626227acf2ac4dc9d42305863286423
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-18 12:29:09 +0900

    bootstrapのドロップダウンの不具合をなくすためにjsファイルのturbolinksを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,12 +9,12 @@
 //= require bootstrap-sprockets
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 
 import "../stylesheets/application"
