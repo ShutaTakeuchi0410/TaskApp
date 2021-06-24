0016ea9a6fd92d5bdb5e5d5bf24fad594098d3a2
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-24 14:25:44 +0900

    Google認証リンクの見た目を整えた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
+
+  # メールアドレスのアカウント名の部分のみを抽出
+  def get_username_from_email
+    arr_email = current_user.email.rpartition("@")
+    @user_name = arr_email[0]
+  end
 end
