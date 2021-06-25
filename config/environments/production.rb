90818a574d12b2463f5ef1836abd03adf21e64c6
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-25 17:25:38 +0900

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # Precompiling assets failed.で変更と追加
-  config.assets.compile = true
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
