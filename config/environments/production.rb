ca8e33a5689ee50cb1d8a484d376d58f9d71bb1c
Shuta Takeuchi <takeuchi@hab-co.jp>
2021-06-29 12:02:55 +0900

    productionの方にもActionMailerの設定を追加した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,21 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  # メール関連
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",  # <======= 送信メールサーバーのアドレスを入れる
+    port: 587,
+    domain: "domain.of.sender.net",  # <===== ぶっちゃけ何でもいい
+    authentication: "plain",
+    user_name: ENV['MAIL_ADDRESS'], # <===== 送信するメールアカウント
+    password: ENV['MAIL_PASSWORD'],    # <===== 送信するメールアカウントのパスワード
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
